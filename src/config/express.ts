d9920dc5c714bc8f81e1f8aa6dc3df3fbd121068
ItayPessach <etaypess@gmail.com>
2024-01-19 15:27:21 +0200

    add cors middleware

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,13 @@
 import { Express } from "express";
 import bodyParser from "body-parser";
+import cors from 'cors';
 import { configRoutes } from './routes';
 import { configSwagger } from './swagger';
 export const configExpress = (app: Express) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
+
     configRoutes(app);
     if (process.env.NODE_ENV === 'development') {
         configSwagger(app);
